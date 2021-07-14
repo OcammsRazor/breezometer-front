22f9e42f43ced1a8d16ad66a65230f7c79f12c54
Shachar <60000798+OcammsRazor@users.noreply.github.com>
2021-07-14 15:54:55 +0300

    implemented card and badge

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import Badge from '../badge/Badge'
+import './Card.css'
 
 class Card extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            locationsList: []
+        }
+    }
+
+    componentWillMount() {
+        fetch('http://localhost:3000/locations/getLocationsList').then(response => response.json())
+        .then(locations => {
+            this.setState({locationsList: locations})
+        })
     }
 
     render() {
-      return <h1>Hello, {this.props.name}</h1>;
+        return (
+            <div className='card-wrapper'>
+                {this.state.locationsList.map(location => (
+                    <Badge name={location.name}></Badge>
+                ))}
+            </div>
+        );
     }
-  }
+}
 
 export default Card;
