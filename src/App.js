a1599d0ab5097c9f8edc842cf5dbae8f6766f142
Shachar <60000798+OcammsRazor@users.noreply.github.com>
2021-07-14 16:55:02 +0300

    added user support and formdata support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import './App.css';
-import Card from './components/card/Card'
+import Card from './components/card/Card';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Breezometer excercise</h1>
-      <Card name="asd"></Card>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: 1
+    }
+
+    this.users = [
+      { id: 1, name: 'Shachar' },
+      { id: 2, name: 'Dor' },
+      { id: 3, name: 'Michael' }
+    ];
+  }
+
+  selectNewUser(newUser) {
+    this.setState({ currentUser: newUser })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>Breezometer excercise</h1>
+        <h3>who are you?</h3>
+        {this.users.map((user, i) => {
+          return(<button key={i} onClick={() => this.selectNewUser(user.id)}>{user.name}</button>)
+        })}
+        <br></br>
+        <Card user={this.state.currentUser}></Card>
+      </div>
+    );
+
+  }
 }
 
 export default App;
